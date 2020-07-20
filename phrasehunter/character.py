9d0fe89f3f470131a2101144138d537a41238762
Joe Michelini <josephmichelini@gmail.com>
2020-07-20 14:51:33 -0400

    add inital logic for character and phrase class

--- phrasehunter/character.py
+++ phrasehunter/character.py
@@ -1,1 +1,19 @@
 # Create your Character class logic in here.
+
+class Character:
+    def __init__(self, original, was_guessed=False):
+        self.original = original
+        self.was_guessed = was_guessed
+
+    def check_guess(self, guess):
+        if guess == self.original:
+            self.was_guessed = True
+
+    def display_char(self):
+        if self.original == " ":
+            self.was_guessed = True
+
+        if self.was_guessed == True:
+            return self.original
+        else:
+            return "_"
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,20 @@
 # Create your Phrase class logic here.
+#from phrasehunter.character import Character
+
+
+class Phrase(list):
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+        for char in self.phrase:
+            self.append(Character(char))
+
+    def eval_guess(self, guess):
+        for char in self:
+            char.check_guess(guess)
+
+    def print_phrase(self):
+        display_string = ""
+        for char in self:
+            display_string += char.display_char()
+        print(display_string)
