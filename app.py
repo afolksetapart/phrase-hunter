6e3d6845f98863895d7b8031a55557d301869ef7
Joe Michelini <josephmichelini@gmail.com>
2020-07-21 07:47:51 -0400

    remove initial comments

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-# Import your Game class
+
 from phrasehunter.game import Game
-# Create your Dunder Main statement.
+
 phrase_list = ["an apple a day", "the more the merrier", "hair in a biscuit",
                "lost at sea", "get what you give", "head in the clouds", "dancing in the moonlight"]
 
@@ -8,6 +8,3 @@ phrase_list = ["an apple a day", "the more the merrier", "hair in a biscuit",
 if __name__ == "__main__":
     game = Game(phrase_list)
     game.start_game()
-# Inside Dunder Main:
-# Create an instance of your Game class
-# Start your game by calling the instance method that starts the game loop
